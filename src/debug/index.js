b70d88378feef7deb26ed416ffbe8ce204947a27
Camilo Beltran Fonseca <cambelf.dev@gmail.com>
2021-07-23 14:48:58 -0500

    Agregada la ultima seccion, Debuggin

--- src/debug/index.js
+++ src/debug/index.js
@@ -1,3 +1,6 @@
+// Chrome Devtools permite realizar debuggin de nuestro codigo escribiendo la palabra reservada debugger. debugger detendra la ejecucion del algoritmo y gracias a las devtools nos permitira evaluar el estado de las variables, los datos que almacena y el scope en que estan definidas.
+
+// Debugger dentro de ChromeDevtools se ubica en la pestaña Source
 var a = "Hello";
 
 function hello() {
@@ -10,3 +13,22 @@ function hello() {
 }
 
 hello();
+
+const moneyBox2 = () => {
+  debugger;
+  var saveCoins = 0;
+  console.log(`Dentro de la funcion pero fuera del closure: $${saveCoins}`);
+  const countCoins = (coins) => {
+    debugger;
+    console.log(saveCoins);
+    saveCoins += coins;
+    console.log(`MoneyBox: $${saveCoins}`);
+  };
+  return countCoins;
+};
+
+let myMoneyBox = moneyBox2();
+
+myMoneyBox(4);
+myMoneyBox(6);
+myMoneyBox(10);
